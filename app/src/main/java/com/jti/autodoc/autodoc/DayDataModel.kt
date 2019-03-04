c30a46d482b6f60c5da3dab03ccb362e0cd275e4
Jean Tampon <jean.tampon@gmail.com>
2019-03-04 08:18:01 +0100

    Create base data structure

--- app/src/main/java/com/jti/autodoc/autodoc/DayDataModel.kt
+++ app/src/main/java/com/jti/autodoc/autodoc/DayDataModel.kt
@@ -1,2 +1,11 @@
 package com.jti.autodoc.autodoc
 
+data class DayDataMedoc(val dayID: Int)
+{
+    val medocs: MutableList<MedocData> = ArrayList()
+
+    fun addMedoc(name: String, time: Int)
+    {
+        medocs.add(MedocData(name, time))
+    }
+}
---
+++ app/src/main/java/com/jti/autodoc/autodoc/MedocData.kt
@@ -0,0 +1,6 @@
+package com.jti.autodoc.autodoc
+
+data class MedocData(val name: String, val time: Int)
+{
+
+}
