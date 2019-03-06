2cd8ab03c7679e0ee57653c50691af258bc76db6
Jean Tampon <jean.tampon@gmail.com>
2019-03-06 03:28:59 +0100

    Removed useless imports

--- app/src/main/java/com/jti/autodoc/autodoc/DayDataAdapter.kt
+++ app/src/main/java/com/jti/autodoc/autodoc/DayDataAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.*
-import android.widget.TimePicker
 import android.app.TimePickerDialog
 import java.util.*
 
